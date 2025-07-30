5c5164200240aecf32fbdfd7edd5ffe2700f82b2
Weldon <weldonbarros179@gmail.com>
2025-07-30 10:12:23 -0300

    feat: adicionar o hero section e a biblioteca AOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
