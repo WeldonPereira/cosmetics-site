e4318f49056523f0a16d70a8d0830be5d0e57095
Weldon <weldonbarros179@gmail.com>
2025-07-31 10:46:50 -0300

    feat: adicionar página de serviços

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
     <div>
       <Header />
       <Hero />
+      <Services />
     </div>
   );
 }
